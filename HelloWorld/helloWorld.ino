cbc5dc54c438e87a995df84fd40770200eff2906
Nitish Thatte <nitishsthatte@gmail.com>
2013-04-17 14:20:00 -0400

    Have encoder working with clock circuit. Resetting at high motor voltages problem is back

--- HelloWorld/helloWorld.ino
+++ HelloWorld/helloWorld.ino
@@ -1,10 +1,36 @@
 /* Hello World program */
 
+#include <stdint.h>
+#include <stdio.h>
 
-void setup(){
+int i;
+int led;
+
+void setup()
+{
+    i = 0;
+    led = 13;
+    pinMode(led, OUTPUT);    
+    digitalWrite(led,LOW);
 	Serial.begin(9600);
+    Serial.print("Please enter a number: ");
 }
 
-void loop(){
-    Serial.println("Hello World");
+void loop()
+{
+    if(Serial.available())
+    {
+        i = Serial.parseInt();
+        Serial.println("");
+        Serial.print("You entered: ");
+        Serial.println(i);
+        Serial.print("Please enter a number: ");
+        for(int num = 0; num < i; num++)
+        {
+            digitalWrite(led,HIGH);
+            delay(250);
+            digitalWrite(led,LOW);
+            delay(250);
+        }            
+    }
 }
